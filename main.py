a6c8d7feade66c8053fcbdd415d31ef8a7f4ebdc
Givrain <ortegagivrain@gmail.com>
2024-08-24 08:40:21 -0600

    Segundo avance del proyecto. Implementacion de operadores aritmeticos.

---
+++ main.py
@@ -0,0 +1,36 @@
+from prettytable import PrettyTable
+import datetime
+logo = '''
+
+████████╗░█████╗░░██████╗██╗░░██╗  ███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
+╚══██╔══╝██╔══██╗██╔════╝██║░██╔╝  ████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
+░░░██║░░░███████║╚█████╗░█████═╝░  ██╔████╔██║███████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝
+░░░██║░░░██╔══██║░╚═══██╗██╔═██╗░  ██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
+░░░██║░░░██║░░██║██████╔╝██║░╚██╗  ██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
+░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
+'''
+
+print(logo)
+tabla_tarea = PrettyTable(['Id. de Tarea', 'Descripción', 'Fecha de creación', 'Fecha límite', 'Completada'])
+print(tabla_tarea)
+
+print("Proporcione la fecha límite para realizar esta tarea ")
+
+dia_limite = int(input("Ingrese el día de la fecha límite: "))
+mes_limite = int(input("Ingrese el mes de la fecha límite: "))
+anio_limite =  int(input("Ingrese el año de la fecha límite: "))
+
+fecha_creacion = datetime.datetime.now()
+fecha_limite = datetime.datetime(anio_limite, mes_limite, dia_limite)
+
+dia_creacion = fecha_creacion.date()
+dia_limite = fecha_limite.date()
+
+diferencia_dias = dia_limite - dia_creacion
+
+print(f"Tienes {diferencia_dias.days} días para completar la tarea")
+print(type(diferencia_dias.days))
+
+
+
+
