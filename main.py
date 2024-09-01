5b790954fc3d79762a8e14c203fa3a60c025287a
Givrain <ortegagivrain@gmail.com>
2024-09-01 12:15:39 -0600

    Jugando con archivos de excel y funciones

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
 from prettytable import PrettyTable
-import datetime
-logo = '''
-
-████████╗░█████╗░░██████╗██╗░░██╗  ███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
-╚══██╔══╝██╔══██╗██╔════╝██║░██╔╝  ████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
-░░░██║░░░██ ████║╚█████╗░█████═╝░  ██╔████╔██║██ ████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝
-░░░██║░░░██╔══██║░╚═══██╗██╔═██╗░  ██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
-░░░██║░░░██║░░██║██████╔╝██║░╚██╗  ██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
-░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
-'''
+from tarea import Tarea
+from datetime import datetime
+from openpyxl import Workbook, load_workbook
+from logo import logo
 
 print(logo)
-tabla_tarea = PrettyTable(['Id. de Tarea', 'Descripción', 'Fecha de creación', 'Fecha límite', 'Completada'])
+
+
+wb = load_workbook('C:/Users/Givrain/OneDrive/Escritorio/Primer Semestre - tec/Pensamiento/proyecto/tareas.xlsx')
+ws = wb.active
+
+tarea = Tarea(1,'', '')
+
+tabla_tarea = PrettyTable(['Id. de Tarea', 'Título', 'Fecha de creación', 'Fecha límite', 'Completada'])
+
+def diferencia_dias(fecha_limite, fecha_creacion):
+
+    dia_creacion = fecha_creacion.date()
+    dia_limite = fecha_limite.date()
+    diferencia_dias = dia_limite - dia_creacion
+    print(f"Tienes {diferencia_dias.days} días para completar la tarea")
+
+def agregar_tarea():
+    fecha_creacion = datetime.now()
+
+    print("Proporcione la fecha límite para realizar esta tarea. ")
+
+    dia_limite = int(input("Ingrese el día de la fecha límite: "))
+    mes_limite = int(input("Ingrese el mes de la fecha límite: "))
+    anio_limite =  int(input("Ingrese el año de la fecha límite: "))
+    fecha_limite = datetime(anio_limite, mes_limite, dia_limite)
+
+    diferencia_dias(fecha_limite, fecha_creacion)
+
+    tarea.titulo = input('Inserte el titulo de la tarea: ')
+    tarea.fecha_creacion = fecha_creacion
+    tarea.fecha_limite = fecha_limite
+    ws.append((tarea.id, tarea.titulo, tarea.fecha_creacion, tarea.fecha_limite, tarea.completado))
+    wb.save('C:/Users/Givrain/OneDrive/Escritorio/Primer Semestre - tec/Pensamiento/proyecto/tareas.xlsx')
+
+    
+    
+   
+
+
+print(logo)
 print(tabla_tarea)
+print('')
 
-print("Proporcione la fecha límite para realizar esta tarea ")
+respuesta_usuario = input('¿Qué desea hacer? a: agregar tarea b: eliminar c: editar s: salir ')
 
-dia_limite = int(input("Ingrese el día de la fecha límite: "))
-mes_limite = int(input("Ingrese el mes de la fecha límite: "))
-anio_limite =  int(input("Ingrese el año de la fecha límite: "))
+while respuesta_usuario != 's':
 
-fecha_creacion = datetime.datetime.now()
-fecha_limite = datetime.datetime(anio_limite, mes_limite, dia_limite)
+    if respuesta_usuario == 'a':
+        agregar_tarea()
 
-dia_creacion = fecha_creacion.date()
-dia_limite = fecha_limite.date()
+    respuesta_usuario = input('¿Qué desea hacer? a: agregar tarea b: eliminar c: editar s: salir')
+    
+print('Gracias por usar el programa!')
 
-diferencia_dias = dia_limite - dia_creacion
-
-print(f"Tienes {diferencia_dias.days} días para completar la tarea")
 
 
 
---
+++ tarea.py
@@ -0,0 +1,10 @@
+class Tarea:
+    def __init__(self, titulo, fecha_creacion, fecha_limite):
+        self.id = 0
+        self.titulo = titulo
+        self.fecha_creacion = fecha_creacion
+        self.fecha_limite = fecha_limite
+        self.completado = False
+       
+
+    
