b7d76bf16262a08e6ef445ab9b4a85e2cba7fde7
Givrain <ortegagivrain@gmail.com>
2024-10-20 14:13:47 -0600

    Actualización final del programa y su documentación

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-from prettytable import PrettyTable
-from datetime import datetime
-from openpyxl import load_workbook
-from logo import logo
-from matplotlib import pyplot as plt
+"""Modulo que proporciona una función para obtener"""
 from os.path import abspath
+from datetime import datetime
+from prettytable import PrettyTable
+from openpyxl import load_workbook
+from matplotlib import pyplot as plt
 
-class Tarea:
-    def __init__(self, titulo, fecha_creacion, fecha_limite):
-        self.id = 1
-        self.titulo = titulo
-        self.fecha_creacion = fecha_creacion
-        self.fecha_limite = fecha_limite
-        self.completado = "❌"
-
-absoluta = abspath("proyecto\\tareas.xlsx")
-
-wb = load_workbook(absoluta)
+LOGO = '''
+████████╗░█████╗░░██████╗██╗░░██╗  ███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░
+╚══██╔══╝██╔══██╗██╔════╝██║░██╔╝  ████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗
+░░░██║░░░██ ████║╚█████╗░█████═╝░  ██╔████╔██║██ ████║██╔██╗██║██ ████║██║░░██╗░█████╗░░██████╔╝
+░░░██║░░░██╔══██║░╚═══██╗██╔═██╗░  ██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗
+░░░██║░░░██║░░██║██████╔╝██║░╚██╗  ██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║
+░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝
+'''
+ABSOLUTA = abspath("tareas.xlsx")
+ETIQUETAS = ["Tareas completadas",
+                "Tareas No completadas"]
+EXP = (0.1, 0.1)
+FORMATO_DECIMALES = '%1.1f%%'
+wb = load_workbook(ABSOLUTA)
 ws = wb.active
-tarea = Tarea(1,'', '')
 
-columnas = ['Id. de Tarea', 'Título', 'Fecha de creación', 'Fecha límite', 'Completada']
-tabla_tarea = PrettyTable(columnas)
-    
+COLUMNAS = ['Id. de Tarea', 'Título', 'Fecha de creación', 'Fecha límite', 'Completada']
+tabla_tarea = PrettyTable(COLUMNAS)
+
 def actualizar_tabla():
-     
+    """Función que refresca la tabla y actualiza los identificadores de las tareas ingresadas"""
     actualizar_identificadores()
-      
     tabla_tarea.clear_rows()
     for fila in ws.iter_rows():
         valores = []
@@ -34,103 +35,112 @@ def actualizar_tabla():
         tabla_tarea.add_row(valores)
     print(tabla_tarea)
 
+
 def actualizar_identificadores():
-     identificador = 1
-     for fila in ws.iter_rows(min_row=1, max_col=1):
+    """Funcion que revisa cada identificador de la tabla y lo coloca en orden ascendente"""
+    identificador = 1
+    for fila in ws.iter_rows(min_row=1, max_col=1):
         for celda in fila:
             celda.value = identificador
             identificador+=1
-            wb.save(absoluta)
+            wb.save(ABSOLUTA)
+
 
 def ingrese_fecha_limite():
+    """Se solicita ingresar los datos de fecha para
+     devolver al usuario la misma fecha en otro formato
+     """
     dia_limite = int(input("Ingrese el día de la fecha límite: "))
     mes_limite = int(input("Ingrese el mes de la fecha límite: "))
     anio_limite =  int(input("Ingrese el año de la fecha límite: "))
     return datetime(anio_limite, mes_limite, dia_limite)
 
-def diferencia_dias(fecha_limite, fecha_creacion):
 
+def diferencia_dias(fecha_limite, fecha_creacion):
+    """Encuentra y muestra la diferencia de días entre 
+    la fecha de creación de la tarea y la fecha límite
+     """
     dia_creacion = fecha_creacion.date()
-    dia_limite = fecha_limite.date()
-    diferencia_dias = dia_limite - dia_creacion
-    print(f"Tienes {diferencia_dias.days} días para completar la tarea")
+    dia_limite_tarea = fecha_limite.date()
+    diferencia_de_dias = dia_limite_tarea - dia_creacion
+    print(f"Tienes {diferencia_de_dias.days} días para completar la tarea")
+
 
 def agregar_tarea():
+    """Ingresa una nueva tarea dentro del archivo xlsx"""
     fecha_creacion = datetime.now()
     print("Proporcione la fecha límite para realizar esta tarea. ")
-
     fecha_limite = ingrese_fecha_limite()
-
     diferencia_dias(fecha_limite, fecha_creacion)
+    titulo = input('Inserte el titulo de la tarea: ')
+    ws.append(
+        (0, 
+        titulo,
+        fecha_creacion.strftime("%d-%m-%Y"),
+        fecha_limite.strftime("%d-%m-%Y"),
+        "❌")
+        )
+    wb.save(ABSOLUTA)
 
-    tarea.titulo = input('Inserte el titulo de la tarea: ')
-    tarea.fecha_creacion = fecha_creacion
-    tarea.fecha_limite = fecha_limite
-    ws.append((tarea.id, tarea.titulo, tarea.fecha_creacion.strftime("%d-%m-%Y"), tarea.fecha_limite.strftime("%d-%m-%Y"), tarea.completado))
-    wb.save(absoluta)
 
-#✅
-#❌
-
-def eliminar_fila(id):
+def eliminar_fila(id_tarea):
+    """Elimina nuevas tareas en cada valor de """
     for fila in ws.iter_rows(min_row=1, max_col=1):
         for celda in fila:
-            if celda.value == id:
+            if celda.value == id_tarea:
                 ws.delete_rows(celda.row)
-                wb.save(absoluta)
+                wb.save(ABSOLUTA)
                 return True
+    return False
 
-def editar_fila(id, editar_titulo, editar_fecha, completada):
-    if id <= ws.max_row:
-        
+
+def editar_fila(id_tarea, edicion_titulo, edicion_fecha, tarea_completada):
+    """Pregunta al usuario si quiere modificar cada elemento de la tarea,
+     para luego solicitarle que ingrese los valores.
+     """
+    if id_tarea <= ws.max_row and id_tarea > 0:
         fila = ws[id]
-        if editar_titulo == 's':
+        if edicion_titulo == 's':
             fila[1].value = input("Ingrese el titulo de la tarea: ")
-        if editar_fecha == 's':
+        if edicion_fecha == 's':
             fila[3].value = ingrese_fecha_limite().strftime("%d-%m-%Y")
-        if completada == 's':
+        if tarea_completada == 's':
             fila[4].value = '✅'
         else:
             fila[4].value = '❌'
-
-        wb.save(absoluta)
+        wb.save(ABSOLUTA)
     else:
         print("Ingrese un identificador valido.")
 
+
 def mostrar_estadistica():
-    
-    etiquetas = ["Tareas completadas", "Tareas No completadas"]
+    """Muestra en una gráfica que aparece en una ventana emergente, mostrando el porcentaje
+       de tareas completadas 
+    """
     tareas_totales = ws.max_row
     tareas_completadas = 0
-
     for fila in ws.iter_rows(min_row=1, min_col=5, max_col=5):
         for celda in fila:
             if celda.value == '✅':
                 tareas_completadas+=1
-
     tareas_faltantes = tareas_totales-tareas_completadas
-
     tareas_completadas/=tareas_totales*100
     tareas_faltantes/=tareas_totales*100
     valores = [tareas_completadas, tareas_faltantes]
-    exp = (0.1,0.1)
-    ax1 = plt.subplots()
-    ax1.pie(valores, explode=exp, labels=etiquetas, autopct='%1.1f%%', shadow=True, startangle=90)
-
-    ax1.axis('equal')
+    
+    plt.pie(valores, explode=EXP, labels=ETIQUETAS, autopct=FORMATO_DECIMALES, shadow=True, startangle=90)
     plt.title("Estadísticas de tareas")
     plt.legend()
     plt.show()
 
-print(logo)
+print(LOGO)
 actualizar_tabla()
 print('')
 
-seguir_ejecutando = True
+respuesta_usuario = input('¿Qué desea hacer? a: Agregar tarea | b: Eliminar | \
+c: Editar | e: Mostrar estadística | s: Salir: ')
 
-while seguir_ejecutando:
-
-    respuesta_usuario = input('¿Qué desea hacer? a: Agregar tarea | b: Eliminar | c: Editar | e: Mostrar estadística | s: Salir: ')
+while respuesta_usuario != 's':
 
     if respuesta_usuario == 'a':
         agregar_tarea()
@@ -144,23 +154,22 @@ while seguir_ejecutando:
             print("No se ha podido eliminar la fila :(")
 
     elif respuesta_usuario == 'c':
-
         dato_editar = int(input("Ingrese el Id. del dato que desea modificar: "))
-
         editar_titulo = input('¿Desea editar el titulo? s/n: ')
         editar_fecha = input('¿Desea editar la fecha limite? s/n: ')
         completada = input('¿Ya completaste la tarea? s/n: ')
-
         editar_fila(dato_editar, editar_titulo, editar_fecha, completada)
-    
     elif respuesta_usuario=='e':
         mostrar_estadistica()
-    elif respuesta_usuario == 's':
-        seguir_ejecutando = False
     else:
         print("Seleccione una opción válida.")
+
     actualizar_tabla()
+    respuesta_usuario = input('¿Qué desea hacer? a: Agregar tarea | b: Eliminar | \
+    c: Editar | e: Mostrar estadística | s: Salir: ')
+
 
 wb.close()
 
 print('\x1b[6;30;42m' + 'Gracias por usar el programa!' + '\x1b[0m')
+# End-of-file
